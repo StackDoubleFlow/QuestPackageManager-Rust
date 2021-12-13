9a1a6cbac92859c6612c437f80468caa6660f090
RedBrumbler <redbrumbler.official+github@gmail.com>
2021-12-13 23:47:48 +0100

    Add legacy fix for wrong include paths in packages

--- src/commands/cache.rs
+++ src/commands/cache.rs
@@ -1,9 +1,11 @@
+use std::io::{Read, Write};
+
 use clap::{AppSettings, Clap};
 use owo_colors::OwoColorize;
 use remove_dir_all::remove_dir_contents;
 use walkdir::WalkDir;
 
-use crate::data::config::Config;
+use crate::data::{config::Config, package::PackageConfig};
 
 #[derive(Clap, Debug, Clone)]
 #[clap(setting = AppSettings::ColoredHelp)]
@@ -21,6 +23,8 @@ pub enum CacheOperation {
     List,
     /// Shows you the current cache path
     Path,
+    /// Fixes some dependencies that use technically wrong include paths
+    LegacyFix,
 }
 
 pub fn execute_cache_operation(operation: Cache) {
@@ -28,6 +32,7 @@ pub fn execute_cache_operation(operation: Cache) {
         CacheOperation::Clear => clear(),
         CacheOperation::List => list(),
         CacheOperation::Path => path(),
+        CacheOperation::LegacyFix => legacy_fix(),
     }
 }
 
@@ -61,6 +66,35 @@ fn list() {
                 " - {}",
                 unwrapped.file_name().to_string_lossy().bright_green()
             );
+        }
+    }
+}
+
+fn legacy_fix() {
+    for entry in WalkDir::new(Config::read_combine().cache.unwrap())
+        .min_depth(2)
+        .max_depth(2)
+    {
+        let path = entry.unwrap().into_path().join("src");
+        let package = PackageConfig::read_path(path.join("qpm.json"));
+
+        let shared_path = path.join(package.shared_dir);
+
+        for entry in WalkDir::new(shared_path) {
+            let entry_path = entry.unwrap().into_path();
+            if entry_path.is_file() {
+                let mut file = std::fs::File::open(entry_path).expect("Opening qpm.json failed");
+                let mut buf: String = "".to_string();
+                file.read_to_string(&mut buf).unwrap();
+                file.write_all(
+                    buf.replace(
+                        "#include \"extern/beatsaber-hook/",
+                        "#include \"beatsaber-hook/",
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            }
         }
     }
 }
