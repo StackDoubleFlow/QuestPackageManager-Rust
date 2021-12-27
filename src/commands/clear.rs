dff72b2006edd03b650c532f05565702190f4ef0
RedBrumbler <redbrumbler.official+github@gmail.com>
2021-12-27 13:07:01 +0100

    Fix for #15

--- src/commands/clear.rs
+++ src/commands/clear.rs
@@ -14,8 +14,12 @@ pub fn remove_dependencies_dir() {
     if std::path::Path::new(&package.dependencies_dir).exists() {
         for entry in WalkDir::new(package.dependencies_dir.canonicalize().unwrap()).min_depth(1) {
             let path = entry.unwrap().into_path();
+            #[cfg(debug_assertions)]
+            println!("Path: {}", path.display().bright_yellow());
             if path.is_symlink() {
                 if path.is_dir() {
+                    #[cfg(debug_assertions)]
+                    println!("Was symlink dir!");
                     if let Err(e) = symlink::remove_symlink_dir(&path) {
                         println!(
                             "Failed to remove symlink for directory {}: {}",
@@ -24,15 +28,30 @@ pub fn remove_dependencies_dir() {
                         );
                     }
                 } else if path.is_file() {
+                    #[cfg(debug_assertions)]
+                    println!("Was symlink file!");
                     if let Err(e) = symlink::remove_symlink_file(&path) {
                         println!(
-                            "Failed to remove symlink for directory {}: {}",
+                            "Failed to remove symlink for file {}: {}",
                             path.display().bright_yellow(),
                             e
                         );
                     }
+                } else {
+                    #[cfg(debug_assertions)]
+                    println!("Was broken symlink!");
+                    if let Err(ed) = std::fs::remove_dir(&path) {
+                        if let Err(ef) = std::fs::remove_file(&path) {
+                            println!(
+                            "Failed to remove broken symlink for {}:\nAttempt 1 (dir):{}\nAttempt 2 (file):{}",
+                            path.display().bright_yellow(),
+                            ed,
+                            ef
+                        );
+                        }
+                    }
                 }
-            };
+            }
         }
 
         remove_dir_all(&package.dependencies_dir).expect("Failed to remove cached folders");
